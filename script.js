d77d56e6c5b6164df199f068ae8553c4e65ed779
sahmed37 <156940147+sahmed37@users.noreply.github.com>
2024-04-03 15:54:07 +0000

    Lab 11 - partial comments

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ COMP 125 Lab 11 - Adding to an Array with mouse clicks
 Step 1 - add a line of code to function mousePressed() that will add a new object to the array when the mouse is pressed. The first part (creating a new object where the mouse is pressed) is done for you. Just add the line that appends it to the array.
 Step 2 - add comments explaining each line of code in this sketch.
 */
-var dots = new Array(1);
+var dots = new Array(1); //create a new array of one element and assign it to "dot"
 
-function setup() {
-  createCanvas(500, 300); 
-  dots[0] = new Dot(width/2, height/2);
-}
+function setup() { //runs once
+  createCanvas(500, 300); // creates p5 canvas
+  dots[0] = new Dot(width/2, height/2); //initilize the first setting of the dot
+} // end the setup function
 
 function draw() {
   background(200);
   
   for(let i = 0; i < dots.length; i++){
-    dots[i].move(); 
-    dots[i].display(); 
+    dots[i].move(); //makes dots move
+    dots[i].display(); //makes dots appear
   }
   textSize(24);
   fill(100, 0, 200);
@@ -24,8 +24,8 @@ function draw() {
 
 }
 
-function mousePressed(){
-  let obj = new Dot(mouseX, mouseY);
+function mousePressed(){ //different from "if mousePressed"
+  let obj = new Dot(mouseX, mouseY); //dots will appear anywhere you click mouse
   dots.push(obj);
   
   /* add a line of code that adds "obj" to the "dots" array. Use the .push() method of the dots array object to append the new "obj" to the end of an existing array. Check the Array.push() documentation first to make sure you are using it correctly.
